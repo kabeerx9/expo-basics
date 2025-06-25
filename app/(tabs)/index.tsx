b1028164a777b88e714844fc0a8066c2c09cf61b
kabeerx9 <kabeer786joshi@gmail.com>
2025-06-25 23:21:34 +0530

    temp

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,13 @@
 import { Stack } from 'expo-router';
 
-import { StyleSheet, View } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function Home() {
   return (
     <>
       <Stack.Screen options={{ title: 'Tab One' }} />
       <View style={styles.container}>
-        <ScreenContent path="app/(tabs)/index.tsx" title="Tab One" />
+        <Text>This is the first tab screen content</Text>
       </View>
     </>
   );
--- app/modal.tsx
+++ app/modal.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform } from 'react-native';
-
-import { ScreenContent } from '~/components/ScreenContent';
+import { Platform, Text, View } from 'react-native';
 
 export default function Modal() {
   return (
     <>
-      <ScreenContent path="app/modal.tsx" title="Modal"></ScreenContent>
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+      <View>
+        <Text>This is the modal content</Text>
+      </View>
     </>
   );
 }
